ae328ee5d2cd37424e378265b91c8ac84a611b32
Ariel Marte <ariel.amas@hotmail.com>
2023-01-11 15:53:26 -0300

    Configuração Inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/home.module.scss';
+//import { Inter } from '@next/font/google'
+//const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <h1 className={styles.title}> hello world</h1>
+  )
+}
