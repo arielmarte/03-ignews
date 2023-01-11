4e424e1b2ffbe5f78934cc99c9c3165aa91e1ef0
Ariel Marte <ariel.amas@hotmail.com>
2023-01-11 17:05:01 -0300

    Estilos Globais | Estrutura da Aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import styles from '../styles/home.module.scss';
-//import { Inter } from '@next/font/google'
-//const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}> hello world</h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>
+        hello world
+      </h1>
+    </>
+
   )
 }
